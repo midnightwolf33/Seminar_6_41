802e8dfdde8e8e380227560ecdf6f120c309b7f6
midnight33 <gra33@bk.ru>
2023-03-17 18:53:34 +0300

    Задача 41

--- Program.cs
+++ Program.cs
@@ -1,2 +1,25 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Задача 41: Пользователь вводит с клавиатуры M чисел. Посчитайте, сколько чисел больше 0 ввёл пользователь.
+// 0, 7, 8, -2, -2 -> 2
+// 1, -7, 567, 89, 223-> 3
+
+string ReadDataStr(string msg)
+{
+    Console.WriteLine(msg);
+    return Console.ReadLine() ?? "0";
+}
+
+int CountPositivNumber(string str)
+{
+    int count = 0;
+    string strWoSp = str.Replace(" ", "");
+    string[] strArr = strWoSp.Split(",");
+    int[] intArr = new int[strArr.Length];
+    for (int i = 0; i < intArr.Length; i++) intArr[i] = int.Parse(strArr[i]);
+    for (int i = 0; i < intArr.Length; i++) if (intArr[i] > 0) count++;
+
+    return count;
+}
+
+string sequenceNumbers = ReadDataStr("Введите последовательность чисел: ");
+int countPosNum = CountPositivNumber(sequenceNumbers);
+Console.WriteLine("Количество положительных чисел равно: " + countPosNum);
